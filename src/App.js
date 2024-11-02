929701bb3a72e57b3b4184b4fadf973f14a816e2
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2024-11-02 19:18:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import ExploreMenu from './components/ExploreMenu/ExploreMenu';
 import { useState } from 'react';
 import SaleDisplay from './components/SaleDisplay/SaleDisplay';
 import SaleItem from './components/SaleItem/SaleItem';
+import { StoreProvider } from './context/StoreContext';
+
 
 function App() {
   const [category, setCategory] = useState("All");
   return (
+  <StoreProvider>
     <div className="App">
       <Navbar/>
       <Header />
       <ExploreMenu category={category} setCategory={setCategory}/>
-      <SaleDisplay category={category}/>
+      <SaleDisplay/>
       <SaleItem/>
+        
     </div>
+    </StoreProvider>
+   
   );
 }
 export default App
