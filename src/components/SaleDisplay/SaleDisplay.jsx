42902091e05e95888ae3a27a867baa96ce176329
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2024-11-02 19:16:54 +0200

    Update SaleDisplay.jsx

--- src/components/SaleDisplay/SaleDisplay.jsx
+++ src/components/SaleDisplay/SaleDisplay.jsx
@@ -1,24 +1,26 @@
-//import React, { useContext } from "react";
+import React, { useContext } from "react";
 import "./SaleDisplay.css";
-//import { StoreContext } from "../../context/StoreContext";
-//import SaleItem from "../SaleItem/SaleItem";
-///   {sale_list.map((item, index) =>{
- // return <SaleItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
-          
-//})}
+import { StoreContext } from "../../context/StoreContext";
+import SaleItem from "../SaleItem/SaleItem";
 
-
-const SaleDisplay = ({category}) => {
- // const { sale_list} = useContext(StoreContext) // Fallback empty array for sale_list
+const SaleDisplay = ({ category }) => {
+  const { sale_list = [] } = useContext(StoreContext) || {}; // Default to an empty array
 
   return (
     <div className="sale-display" id="sale-display">
-      <h2>Top Featuring </h2>
+      <h2>BRAND NEW LAPTOPS</h2>
       <div className="sale-display-list">
-       
- 
+        {sale_list.map((item, index) => (
+          <SaleItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
       </div>
-      
     </div>
   );
 };
